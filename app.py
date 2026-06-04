7888fa7a15bbac902c3e5d89feac6303c4984c07
jhadas1505-hue <jhadas1505@gmail.com>
2026-06-04 22:25:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="FCOT Chemical Storage Checker",
+    page_title="jihad hansome",
     page_icon="🧪",
     layout="wide"
 )
